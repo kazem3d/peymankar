938604978c4f3991d34c3f01d620230362d9609c
kazem3d <kazem3d@gmail.com>
2020-02-17 23:59:36 +0330

    add tolbar\'s buttom and add base.html

--- Accounting/urls.py
+++ Accounting/urls.py
@@ -3,6 +3,6 @@ from Accounting import views
 
 app_name ='accounting'
 urlpatterns=[
-    path('record_list/',views.record_list),
-    path('record_details/<int:record_id>/',views.record_details,name='record_list')
+    path('record_list/',views.record_list,name="record_list"),
+    path('record_details/<int:record_id>/',views.record_details,name='record_details')
 ] 
