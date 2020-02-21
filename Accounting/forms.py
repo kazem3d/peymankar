cb1a44b83610c7d1d9a8451ac02597a41a221f33
kazem3d <kazem3d@gmail.com>
2020-02-21 22:35:20 +0330

    search form compelet

--- Accounting/forms.py
+++ Accounting/forms.py
@@ -5,6 +5,6 @@ class SearchForm(forms.Form):
     record_name=forms.CharField(label='عنوان',required=False)
     project_name=forms.ModelChoiceField(label='نام پروژه',
                 queryset=Project.objects.all(),required=False)
-    max_price=forms.IntegerField(label='بیشترین قیمت',required=False)
-    min_price=forms.IntegerField(label='کمترین قیمت',required=False)
+    max_price=forms.IntegerField(label='بیشترین قیمت',min_value=0,required=False)
+    min_price=forms.IntegerField(label='کمترین قیمت',min_value=0,required=False)
     
